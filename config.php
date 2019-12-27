eb35c559f005f736d3641347a5e13ea151201882
AKhirwan Novendi <ahir.malmsneen@gmail.com>
2019-12-27 14:50:53 +0700

    db

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
  * using mysqli_connect for database connection
  */
 
-$databaseHost = 'localhost';
+$databaseHost = 'rm-d9j597s5o40fhrxn066060.mysql.ap-southeast-5.rds.aliyuncs.com';
 $databaseName = 'crud_db';
-$databaseUsername = 'root';
-$databasePassword = 'Padaringan_k383k';
+$databaseUsername = 'admin_halalpedia';
+$databasePassword = 'Admin_123';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 
