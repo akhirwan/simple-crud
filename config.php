60b0b107f573b8611af228223d991233d5279c88
akhirwan <56789259+akhirwan@users.noreply.github.com>
2021-03-03 16:46:18 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  * using mysqli_connect for database connection
  */
 
-$databaseHost = 'rm-d9j597s5o40fhrxn0ao.mysql.ap-southeast-5.rds.aliyuncs.com';
+$databaseHost = 'localhost';
 $databaseName = 'crud_db';
 $databaseUsername = 'superadmin';
 $databasePassword = 'Admin_123';
